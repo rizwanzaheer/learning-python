855e34041b2a051de6d47bbf8fb1955ead5cfdd9
Rizwan Zaheer <rizwanzaheerr@gmail.com>
2019-04-14 15:02:09 +0500

    inital learning

---
+++ app.py
@@ -0,0 +1,37 @@
+print("hello Rizwan Zaheer")
+price = 20
+print(price)
+price = 10
+print(price)
+
+# name = input('What is your name? \n')
+#
+# print('\n')
+# print('Hi ' + name)
+#
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+#
+# print('Your age is: ' + str(age))
+
+email = '''
+Hi Rizwan
+this is a multiple line string  
+'''
+
+email = 'Python course'
+
+print(email[-2])
+print(email[:4])
+print(email[2:4])
+print(email[2:])
+print(email[:])
+print(email[1:-1])
+
+fname = 'Rizwan'
+lname = 'Zaheer'
+# formatted string
+msg = f'[{fname} {lname}] is a coder'
+print(msg)
+print(len(msg))
